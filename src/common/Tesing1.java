69f0b9c1427397fc3f5871bc0c8d8142a61f63a1
kyenugu <kyenugu@XHLT0115.xeno.com>
2018-12-21 18:25:03 +0530

    New

---
+++ src/common/Tesing1.java
@@ -0,0 +1,30 @@
+package common;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Tesing1 extends DriverInitializer  {
+ 	
+@Test
+  public void f() {
+		
+    	getDriver().findElement(By.id("TxtEmail")).sendKeys("admin2@gmail.com");
+		getDriver().findElement(By.id("TxtPassword")).sendKeys("xeno@123");
+		getDriver().findElement(By.id("BtnLogin")).click();
+		getDriver().findElement(By.id("cpBody_gvParentGrid_lnk_0")).click();
+		System.out.println("github");
+  }
+	
+	@AfterClass
+	public void close(){
+		getDriver().quit();
+		dr.set(null);
+	}
+	
+}
